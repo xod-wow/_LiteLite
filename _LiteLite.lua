f9f04c5e8cca91886e0b79a11fe0b20eb4b8dcc4
Mike "Xodiv" Battersby <mib@post.com>
2026-03-15 16:55:28 +1100

    Don't error delve list if no vault yet

--- _LiteLite.lua
+++ _LiteLite.lua
@@ -2411,34 +2411,38 @@ function _LiteLite:ListDelves()
             end
         end)
 
+    _LiteLiteTable:Reset()
+    _LiteLiteTable:Setup(DELVES_LABEL, { "Map", "Delve", "Story", "Bountiful?" })
+
     -- Seems to be no way to get a list of delve runs the way you can with M+
     -- Figure out what the highest ilevel reward is and see how many we've done
     -- that would give that reward. Relies on the a vault slot showing more than
     -- the threshold until you complete the next one.
 
     local activities = C_WeeklyRewards.GetActivities(Enum.WeeklyRewardChestThresholdType.World)
-    local activityTierID = activities[1].activityTierID
     local maxItemLevel
-    for i = 1, 32 do
-        local _, _, _, itemLevel = C_WeeklyRewards.GetNextActivitiesIncrease(activityTierID, i)
-        maxItemLevel = itemLevel or maxItemLevel
-    end
-
     local progress = 0
-    for _, info in ipairs(activities) do
-        local itemLink = C_WeeklyRewards.GetExampleRewardItemHyperlinks(info.id)
-        local itemLevel = itemLink and C_Item.GetDetailedItemLevelInfo(itemLink)
-        if itemLevel == maxItemLevel then progress = info.progress end
+    if next(activities) then
+        local activityTierID = activities[1].activityTierID
+        for i = 1, 32 do
+            local _, _, _, itemLevel = C_WeeklyRewards.GetNextActivitiesIncrease(activityTierID, i)
+            maxItemLevel = itemLevel or maxItemLevel
+        end
+
+        for _, info in ipairs(activities) do
+            local itemLink = C_WeeklyRewards.GetExampleRewardItemHyperlinks(info.id)
+            local itemLevel = itemLink and C_Item.GetDetailedItemLevelInfo(itemLink)
+            if itemLevel == maxItemLevel then progress = info.progress end
+        end
+
+        local completedJourney = C_QuestLog.IsQuestFlaggedCompleted(86371)
+        local progText = WHITE_FONT_COLOR:WrapTextInColorCode(string.format("%d/%d", progress, activities[3].threshold))
+        local journeyColor = completedJourney and GREEN_FONT_COLOR or RED_FONT_COLOR
+        local journeyText = journeyColor:WrapTextInColorCode(tostring(completedJourney))
+        local footer = string.format("Max level delves: %s. Journey: %s", progText, journeyText)
+        _LiteLiteTable:SetFooter(footer)
     end
 
-    local completedJourney = C_QuestLog.IsQuestFlaggedCompleted(86371)
-    local progText = WHITE_FONT_COLOR:WrapTextInColorCode(string.format("%d/%d", progress, activities[3].threshold))
-    local journeyColor = completedJourney and GREEN_FONT_COLOR or RED_FONT_COLOR
-    local journeyText = journeyColor:WrapTextInColorCode(tostring(completedJourney))
-    local footer = string.format("Max level delves: %s. Journey: %s", progText, journeyText)
-    _LiteLiteTable:Reset()
-    _LiteLiteTable:Setup(DELVES_LABEL, { "Map", "Delve", "Story", "Bountiful?" })
-    _LiteLiteTable:SetFooter(footer)
     _LiteLiteTable:SetEnableSort(true)
     _LiteLiteTable:SetRows(delveData)
     _LiteLiteTable:Show()
