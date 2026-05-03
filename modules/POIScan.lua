b8edf67cf91076f4fc7cff37583db14caf6d8c65
Mike "Xodiv" Battersby <mib@post.com>
2026-05-03 12:59:02 +1000

    Add POIScan

---
+++ modules/POIScan.lua
@@ -0,0 +1,126 @@
+-- Scan POI for things, alert sounds
+
+local _, addon = ...
+
+local Scanner = CreateFrame('Frame')
+Scanner:SetScript('OnEvent', function (self, ...) self:OnEvent(...) end)
+
+function Scanner:OnEvent(event)
+    if event == "AREA_POIS_UPDATED" then
+        self:ScanAllPOI()
+    end
+end
+
+function Scanner:POIList()
+    addon.printf("Scan for POI:")
+    if next(addon.db.scanPOI or {}) then
+        local names = GetKeysArray(addon.db.scanPOI)
+        table.sort(names)
+        for i, name in ipairs(names) do
+            addon.printf("%d. %s", i, name)
+        end
+    else
+        addon.printf("   None.")
+    end
+end
+
+function Scanner:POIWipe()
+    addon.db.scanPOI = table.wipe(addon.db.scanPOI or {})
+    self:UpdateScanning()
+end
+
+function Scanner:POIAdd(name)
+    addon.db.scanPOI = addon.db.scanPOI or {}
+    addon.db.scanPOI[name:lower()] = {}
+    self:UpdateScanning()
+end
+
+function Scanner:POIDel(name)
+    if addon.db.scanPOI then
+        local n = tonumber(name)
+        if n then
+            local names = GetKeysArray(addon.db.scanPOI)
+            name = names[n]
+        end
+        addon.db.scanPOI[name] = nil
+        self:UpdateScanning()
+    end
+end
+
+function Scanner:UpdateScanning()
+    if next(addon.db.scanPOI or {}) then
+        self.seenPOI = self.seenPOI or {}
+        self:RegisterEvent("AREA_POIS_UPDATED")
+        self:ScanAllPOI()
+    else
+        self:UnregisterAllEvents()
+        self.seenPOI = table.wipe(self.seenPOI or {})
+    end
+end
+
+function Scanner:POIMatches(text, info)
+    text = text:lower()
+    if text:sub(1,1) == '^' then
+        if info.name:lower():find(text) then
+            return true
+        elseif info.atlasName:lower():find(text) then
+            return true
+        end
+    else
+        if info.name:lower():find(text, nil, true) then
+            return true
+        elseif info.atlasName:lower():find(text, nil, true) then
+            return true
+        end
+    end
+    return false
+end
+
+function Scanner:ScanPOI(map, id)
+    local guid = string.format('%d.%d', map, id)
+    if self.seenPOI[guid] then
+        return
+    end
+
+    local info = C_AreaPoiInfo.GetAreaPOIInfo(map, id)
+    if not info then
+        return
+    end
+
+    for n in pairs(addon.db.scanPOI) do
+        if self:POIMatches(n, info) then
+            addon.printf("POI found: %s", info.name)
+            addon.printf("  atlas %s", info.atlasName)
+            PlaySound(11466)
+            self.seenPOI[guid] = true
+        end
+    end
+end
+
+function Scanner:ScanAllPOI()
+    local map = C_Map.GetBestMapForUnit('player')
+    for _, id in ipairs(C_AreaPoiInfo.GetEventsForMap(map)) do
+        self:ScanPOI(map, id)
+    end
+end
+
+local function SlashCommand(arg)
+    local arg1, arg2 = string.split(' ', arg or '', 2)
+    if arg1 == 'add' then
+        Scanner:POIAdd(arg2)
+    elseif arg1 == 'del' then
+        Scanner:POIDel(arg2)
+    elseif arg1 == 'wipe' then
+        Scanner:POIWipe()
+    end
+    Scanner:POIList()
+end
+
+local moduleInfo = {
+    Initialize = function () Scanner:UpdateScanning() end,
+    SlashCommands = {
+        ['poi-scan'] = SlashCommand,
+        ['ps'] = SlashCommand,
+    }
+}
+addon.RegisterModule(moduleInfo)
