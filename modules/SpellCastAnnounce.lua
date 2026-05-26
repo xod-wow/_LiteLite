e8fa9dfdd346e3dd7290825624d4b951373f088b
Mike "Xodiv" Battersby <mib@post.com>
2026-05-26 13:04:41 +1000

    Fix issue with spellcast announce and delves

--- modules/SpellCastAnnounce.lua
+++ modules/SpellCastAnnounce.lua
@@ -1,22 +1,34 @@
 -- Print a message when you cast a specific spell
 
-local function SpellCastAnnounce(_ownerID, unit, _, spellID)
-    if unit ~= 'player' then
-        return
-    end
-
-    if not IsInInstance() or UnitIsPVP('player') or IsActiveBattlefieldArena() then
-        return
-    end
-
+local function ShouldAnnounce()
     local ok, result = pcall(C_ChatInfo.InChatMessagingLockdown)
     if ok and result then
+        return false
+    end
+
+    if UnitIsPVP('player') or IsActiveBattlefieldArena() then
+        return false
+    end
+
+    local _, instanceType = IsInInstance()
+    if instanceType == 'party' then
+        return true
+    elseif instanceType == 'raid' then
+        return true
+    end
+
+    return false
+end
+
+local function SpellCastAnnounce(_ownerID, unit, _, spellID)
+    if unit ~= 'player' or not ShouldAnnounce() then
         return
     end
 
     if spellID == 1231411 then  -- Recuperate
         C_ChatInfo.SendChatMessage('Re-cu-per-ate.', 'SAY')
     end
+
 --[[
     if spellID == 115310 then
         -- Revival (Mistweaver Monk)
