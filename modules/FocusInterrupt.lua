27fdf3ff97b02764ba2a865760dd14f174ccbe19
Mike "Xodiv" Battersby <mib@post.com>
2026-05-29 17:03:31 +1000

    Maintain a global interrupt macro

--- modules/FocusInterrupt.lua
+++ modules/FocusInterrupt.lua
@@ -24,6 +24,42 @@ local _, addon = ...
 -- This is compatible with FocusKick. The marker used is your position in the
 -- name-sorted list of party members.
 
+local GlobalMacroName = "Kick"
+
+local GlobalMacroBody = [[#showtooltip
+/focus [mod,@mouseover,harm,nodead][mod,harm,nodead]
+/tm [@focus,mod] {markIndex}
+/stopmacro [mod]
+/cast [@focus,harm] {spellName}; {spellName}]]
+
+local Interrupts = {
+    [ 47528] = true,                -- Mind Freeze (Death Knight)
+    [183752] = true,                -- Disrupt (Demon Hunter)
+    [ 78675] = true,                -- Solar Beam (Druid)
+    [106839] = true,                -- Skull Bash (Druid)
+    [147362] = true,                -- Counter Shot (Hunter)
+    [187707] = true,                -- Muzzle (Hunter)
+    [  2139] = true,                -- Counterspell (Mage)
+    [116705] = true,                -- Spear Hand Strike (Monk)
+    [ 96231] = true,                -- Rebuke (Paladin)
+    [ 15487] = true,                -- Silence (Priest)
+    [  1766] = true,                -- Kick (Rogue)
+    [ 57994] = true,                -- Wind Shear (Shaman)
+    [119910] = true,                -- Spell Lock (Warlock Felhunter Pet)
+    [132409] = true,                -- Spell Lock (Warlock Fel Ravager)
+    [119914] = true,                -- Axe Toss (Warlock Felguard Pet)
+    [  6552] = true,                -- Pummel (Warrior)
+    [351338] = true,                -- Quell (Evoker)
+}
+
+local function GetInterruptSpellName()
+    for spellID in pairs(Interrupts) do
+        if C_SpellBook.IsSpellKnown(spellID) then
+            return C_Spell.GetSpellName(spellID)
+        end
+    end
+end
+
 local function HasInterrupt()
     local spec = GetSpecialization()
     if GetSpecializationRole(spec) ~= "HEALER" then
@@ -114,9 +150,26 @@ local function UpdateMarkMacros()
     end
 end
 
+local function CreateOrUpdateGlobalMacro()
+    if HasInterrupt() then
+        local replacements = {
+            spellName = GetInterruptSpellName(),
+            markIndex = GetMyMark(),
+        }
+        local body = string.gsub(GlobalMacroBody, '{(.-)}', replacements)
+        local macroIndex = GetMacroIndexByName(GlobalMacroName)
+        if macroIndex > 0 then
+            EditMacro(macroIndex, nil, nil, body)
+        else
+            CreateMacro(GlobalMacroName, 134400, body)
+        end
+    end
+end
+
 local function Initialize()
     EventRegistry:RegisterFrameEventAndCallback('GROUP_ROSTER_UPDATE', UpdateMarkMacros)
     EventRegistry:RegisterFrameEventAndCallback('READY_CHECK', NotifyMark)
+    CreateOrUpdateGlobalMacro()
 end
 
 local moduleInfo = {
