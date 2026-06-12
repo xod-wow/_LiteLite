f8e2b2a2af1c39a8dd97f6f002ff41a33f9c6629
Mike "Xodiv" Battersby <mib@post.com>
2026-06-12 17:09:02 +1000

    Make the generic trait handler generic

--- modules/ShowGenericTrait.lua
+++ modules/ShowGenericTrait.lua
@@ -3,11 +3,33 @@
 
 local _, addon = ...
 
+local function Handle(arg)
+    if not arg or arg == '' then
+        GenericTraitUI_LoadUI()
+        for id = 1, 2000 do
+            local info = GenericTraitUtil.GetFrameLayoutInfo(id)
+            if info and info.Title then
+                addon.printf('%d: %s', id, info.Title)
+            end
+        end
+    elseif tonumber(arg) then
+        TraitUtil.OpenTraitFrame(arg)
+    else
+        GenericTraitUI_LoadUI()
+        for id = 1, 2000 do
+            local info = GenericTraitUtil.GetFrameLayoutInfo(id)
+            if info and info.Title and info.Title:find(arg) then
+                TraitUtil.OpenTraitFrame(id)
+                return
+            end
+        end
+    end
+end
+
 local moduleInfo = {
     SlashCommands = {
-        ['drive'] = function () TraitUtil.OpenTraitFrame(1056) end,
-        ['loa'] = function () TraitUtil.OpenTraitFrame(1166) end,
-        ['void'] = function () TraitUtil.OpenTraitFrame(1180) end,
+        ['generic-traits'] = Handle,
+        ['gt'] = Handle,
     }
 }
 addon.RegisterModule(moduleInfo)
